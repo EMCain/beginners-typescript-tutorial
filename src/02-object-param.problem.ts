5fffd895d67c92bbc55a93b1a400a72c14ebd1de
EMCain <11471197+EMCain@users.noreply.github.com1>
2023-03-25 20:14:38 -0700

    02

--- src/02-object-param.problem.ts
+++ src/02-object-param.problem.ts
@@ -1,6 +1,11 @@
 import { expect, it } from "vitest";
 
-export const addTwoNumbers = (params) => {
+interface paramType {
+  first: number,
+  second: number
+}
+
+export const addTwoNumbers = (params: paramType) => {
   return params.first + params.second;
 };
 
